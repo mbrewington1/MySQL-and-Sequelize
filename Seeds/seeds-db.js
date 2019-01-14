f7e5584582cb5e731a34cdd300e7a00ffeabfd28
Brewington <212598938@logon.ds.ge.com>
2019-01-14 13:40:15 -0500

    Final

--- Seeds/seeds-db.js
+++ Seeds/seeds-db.js
@@ -2,61 +2,61 @@ const db = require("../models");
 
 const items = [
   {
-    product_name: "Uncharted 4",
+    product_name: "NBA 2K",
     department_name: "Video Games",
-    price: 49,
+    price: 50,
     stock_quantity: 150
   },
   {
-    product_name: "DOOM",
+    product_name: "Grand Theft Auto V",
     department_name: "Video Games",
-    price: 59,
+    price: 60,
     stock_quantity: 200
   },
   {
-    product_name: "Crate of Spam",
+    product_name: "Chips and Salsa",
     department_name: "Food and Drink",
-    price: 24,
+    price: 15,
     stock_quantity: 50
   },
   {
-    product_name: "Cool Shades",
+    product_name: "Men's Falcons Pajama bottoms",
     department_name: "Apparel",
-    price: 75,
+    price: 25,
     stock_quantity: 5
   },
   {
-    product_name: "Worn Denim Jeans",
+    product_name: "Women's black leggings",
     department_name: "Apparel",
-    price: 54,
+    price: 25,
     stock_quantity: 35
   },
   {
-    product_name: "Survival Towel",
-    department_name: "Necessities",
+    product_name: "Shower Gel",
+    department_name: "Toiletries",
     price: 42,
     stock_quantity: 42
   },
   {
-    product_name: "Bill and Ted's Excellent Adventure",
-    department_name: "Films",
+    product_name: "Kill Bill",
+    department_name: "Movies",
     price: 15,
     stock_quantity: 25
   },
   {
-    product_name: "Mad Max: Fury Road",
-    department_name: "Films",
+    product_name: "This Christmas",
+    department_name: "Movies",
     price: 25,
     stock_quantity: 57
   },
   {
-    product_name: "Monopoly",
+    product_name: "Scattergories",
     department_name: "Board Games",
     price: 30,
     stock_quantity: 35
   },
   {
-    product_name: "Yahtzee",
+    product_name: "Operation",
     department_name: "Board Games",
     price: 19,
     stock_quantity: 23
